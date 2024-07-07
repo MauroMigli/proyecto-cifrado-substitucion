138ffb0d153ff86dc49e672ec5342e27624256e2
Esteban Contreras <estebancontreras@uc.cl>
2024-07-06 21:20:32 -0400

    Clase que cifra y decifra(?

---
+++ cifrado sustitucion/cifrado_sustitucion.py
@@ -0,0 +1,59 @@
+"""
+Archivo encargado de crear el cifrado 
+"""
+from collections import Counter
+import random 
+import constantes as con 
+
+class CifradoSustitucion:
+    def __init__(self, ruta):
+        archivo = open(ruta)
+        data = archivo.readlines()
+        archivo.close()
+
+        for i in range(len(data)):
+            data[i] = data[i].replace("\n", " ")
+            data[i] = data[i].lower()
+            data[i] = data[i].replace("á", "a")
+            data[i] = data[i].replace("é", "e")
+            data[i] = data[i].replace("í", "i")
+            data[i] = data[i].replace("ó", "o")
+            data[i] = data[i].replace("ú", "u")
+
+        self.esta_cifrado = False
+        
+        self.texto = "".join(data)
+
+    def cifrar(self):
+        if not self.esta_cifrado:
+            cifrado = ""
+            indices = con.indices
+            random.shuffle(indices)
+
+            for letra in self.texto:
+                if letra in con.abecedario:
+                    cifrado += con.abecedario[indices[con.indices_abecedario[letra]]]
+                else:
+                    cifrado += letra
+
+            self.texto = cifrado
+            self.esta_cifrado = True
+            print("Texto cifrado!:", self.texto)
+        else:
+            print("El texto ya está cifrado...")
+
+    def decifrar(self):
+        frecuencias = Counter(letra for letra in self.texto if letra != " ")
+        largo = len(self.texto.replace(" ", ""))
+
+if __name__ == "__main__":
+    instancia = CifradoSustitucion("text.txt")
+
+    print(instancia.texto)
+    instancia.cifrar()
+    instancia.decifrar()
+
+        
+        
+
+
