2f18ebbaa42bfba45a601f7fb21b8814320f63ed
Esteban Contreras <estebancontreras@uc.cl>
2024-07-06 21:20:32 -0400

    Clase que cifra y decifra(?

---
+++ cifrado sustitucion/constantes.py
@@ -0,0 +1,8 @@
+"""
+archivo con constantes utiles
+"""
+indices = [i for i in range(27)]
+abecedario = "abcdefghijklmnñopqrstuvwxyz"
+indices_abecedario = {abecedario[i] : i for i in range(27)}
+
+frecuencias = []
